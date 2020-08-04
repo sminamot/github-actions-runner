2773d8fe7de91d45c9e79837dd78e2d1e4f1b3d2
sminamot <shosfs@gmail.com>
2020-08-04 19:49:02 +0900

    :heavy_plus_sign: always refresh token on remove

--- remove.sh
+++ remove.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-if [ -z "$RUNNER_TOKEN" ]; then
-    registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
-    echo "Requesting registration URL at '${registration_url}'"
+registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
+echo "Requesting registration URL at '${registration_url}'"
 
-    payload=$(curl -sX POST -H "Authorization: token ${GITHUB_TOKEN}" ${registration_url})
-    export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
-fi
+payload=$(curl -sX POST -H "Authorization: token ${GITHUB_TOKEN}" ${registration_url})
+export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 
 ./config.sh remove \
     --unattended \
